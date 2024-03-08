5aaa8e8006db1c4a73fd07795a4d4e04f050972e
laura.harvey <laura.harvey@neighbourly.com>
2024-03-08 16:51:32 +0000

    1.7: unicafe step2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -9,6 +9,24 @@ const App = () => {
   const incrementNeutral = () => setNeutral(neutral + 1);
   const incrementBad = () => setBad(bad + 1);
 
+  const total = () => good + neutral + bad;
+
+  const average = () => {
+    if (total() == 0) {
+      return 0;
+    }
+
+    return (good - bad) / total();
+  }
+
+  const positive = () => {
+    if (total() == 0) {
+      return 0;
+    }
+
+    return (good / total()) * 100;
+  }
+
   return (
     <div>
       <h2>give feedback</h2>
@@ -20,6 +38,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total()}</p>
+      <p>average {average()}</p>
+      <p>positive {positive()} %</p>
     </div>
   )
 }
