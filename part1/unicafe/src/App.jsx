9c7afb719dd799606389e568e2b3082b362fea61
laura.harvey <laura.harvey@neighbourly.com>
2024-03-08 16:38:01 +0000

    1.6: unicafe step1

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const incrementGood = () => setGood(good + 1);
+  const incrementNeutral = () => setNeutral(neutral + 1);
+  const incrementBad = () => setBad(bad + 1);
+
+  return (
+    <div>
+      <h2>give feedback</h2>
+      <Button handleClick={() => incrementGood()} text="good" />
+      <Button handleClick={() => incrementNeutral()} text="neutral" />
+      <Button handleClick={() => incrementBad()} text="bad" />
+
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  )
+}
+
+import Button from "./components/Button.jsx"
+
+export default App
