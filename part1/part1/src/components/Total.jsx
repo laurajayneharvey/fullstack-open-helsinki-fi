08eab81b7b0f9b43f3d862a1286836c73e25be92
laura.harvey <laura.harvey@neighbourly.com>
2024-03-08 16:14:08 +0000

    Exercise 1.3

--- part1/part1/src/components/Total.jsx
+++ part1/part1/src/components/Total.jsx
@@ -1,6 +1,6 @@
 const Total = (props) => {
     return (
-        <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+        <p>Number of exercises {props.part1.exercises + props.part2.exercises + props.part3.exercises}</p>
     )
   }
   
